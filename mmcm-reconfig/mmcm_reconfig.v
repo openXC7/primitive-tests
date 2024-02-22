aff8931c3ec66f9a67c9bdd8af41adedb43daa87
Hans Baier <foss@hans-baier.de>
2024-02-23 06:16:41 +0700

    mmcm_reconfig: use a proper example which demonstrates both integer and fractional changes

--- mmcm-reconfig/mmcm_reconfig.v
+++ mmcm-reconfig/mmcm_reconfig.v
@@ -78,16 +78,19 @@ module mmcm_reconfig (
     wire reconfig_ready;
     wire reconfig_done;
     reg  start_reconfig;
-    reg  [5:0] half_period;
+    reg  [6:0] multiplier;
     wire [3:0] mmcm_debug;
+    reg  [2:0] frac;
+    wire frac_low;
+    assign frac_low = ~frac[2];
 
     xilinx7_mmcm_reconfig reconfig (
         .rst(~locked),
         .locked(mmcm_locked),
 
         // CLKOUT0
-        .CLKOUT0_HIGH_TIME  (half_period),
-        .CLKOUT0_LOW_TIME   (half_period),
+        .CLKOUT0_HIGH_TIME  (6'd10),
+        .CLKOUT0_LOW_TIME   (6'd10),
         .CLKOUT0_PHASE_MUX  (3'd0),
         .CLKOUT0_FRAC       (3'd0),
         .CLKOUT0_FRAC_EN    (1'b0),
@@ -104,12 +107,13 @@ module mmcm_reconfig (
         .CLKOUT6_NO_COUNT   (1'b1),
 
         // CLKFBOUT
-        .CLKFBOUT_HIGH_TIME  (6'd10),
-        .CLKFBOUT_LOW_TIME   (6'd10),
+        .CLKFBOUT_HIGH_TIME  (multiplier[6:1]),
+        .CLKFBOUT_LOW_TIME   (multiplier[6:1] - 1 + multiplier[0]), // if FRAC_EN is high, the factor increases by one, so we have to subtract one here
         .CLKFBOUT_PHASE_MUX  (3'd0),
-        .CLKFBOUT_FRAC       (3'd0),
-        .CLKFBOUT_FRAC_EN    (1'b0),
+        .CLKFBOUT_FRAC       (frac),
+        .CLKFBOUT_FRAC_EN    (1'b1),
         .CLKFBOUT_WF_R       (1'b0),
+        .CLKFBOUT_WF_F       (frac_low), // this needs to be high for frac values of 0, 1, 2, 3
         .CLKFBOUT_EDGE       (1'b0),
         .CLKFBOUT_NO_COUNT   (1'b0),
         .CLKFBOUT_DELAY_TIME (6'b0),
@@ -127,7 +131,7 @@ module mmcm_reconfig (
         .dout(dout),
         .den(den),
         .dwe(dwe),
-        .rst_mmcm(rst_mmcm | ~key),
+        .rst_mmcm(rst_mmcm),
         .drdy(drdy),
 
         // activation
@@ -137,26 +141,26 @@ module mmcm_reconfig (
     );
 
     assign clkout = mmcm_clk;
-    reg [25:0] count   = 0;
+    reg [26:0] count   = 0;
     reg [24:0] r_count = 0;
 
     always @(posedge(pll_clk)) begin
         count <= count + 1;
 
-        if (~locked) half_period <= 6'd10;
-        else if (reconfig_ready & (count == 25'hffffff)) begin
-            half_period <= half_period + 1;
+        if (~locked) {multiplier, frac} <= {7'd20, 3'd0};
+        else if (reconfig_ready & (count == 27'h3ffffff)) begin
+            if (~key) begin
+                if (frac == 3'b111) multiplier <= multiplier + 1;
+                frac <= frac + 1;
+            end
         end
 
-        start_reconfig <= reconfig_ready & (count == 26'h2000000);
+        start_reconfig <= reconfig_ready & (count == 27'h4000000);
     end
 
-    assign led[1] = ~r_count[24];
-    assign led[2] = ~start_reconfig;
-    assign led[3] = ~rst_mmcm;
-    assign led[4] = ~reconfig_ready;
-    
     assign led[0] = ~count[25];
+    assign led[1] = ~r_count[24];
+    assign led[4:2] = ~frac;
 
     always @(posedge(mmcm_clk)) r_count <= r_count + 1;
 
